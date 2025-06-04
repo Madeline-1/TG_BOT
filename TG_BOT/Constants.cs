db383bee817f377e57e588c0121cd63188265dd2
Madeline <dancent124@gmail.com>
2025-06-04 17:32:36 +0300

    Update Constants.cs

--- TG_BOT/Constants.cs
+++ TG_BOT/Constants.cs
@@ -8,11 +8,11 @@ namespace TG_BOT
 {
     public class Constants
     {
-        public static string TimeAddress = "https://localhost:7030/api/songLyrics/GetTime?songId=";
-        public static string FavoritesAddress = "https://localhost:7030/api/songLyrics/GetFavorites";
-        public static string AddFavoriteAddress = "https://localhost:7030/api/songLyrics/AddToFavorites";
-        public static string RemoveFavoriteAddress = "https://localhost:7030/api/songLyrics/RemoveFavorite?index=";
-        public static string UpdateFavoriteAddress = "https://localhost:7030/api/songLyrics/UpdateFavorite?index=";
+        public static string TimeAddress = "https://.../api/songLyrics/GetTime?songId=";
+        public static string FavoritesAddress = "https://.../api/songLyrics/GetFavorites";
+        public static string AddFavoriteAddress = "https://.../api/songLyrics/AddToFavorites";
+        public static string RemoveFavoriteAddress = "https://.../api/songLyrics/RemoveFavorite?index=";
+        public static string UpdateFavoriteAddress = "https://.../api/songLyrics/UpdateFavorite?index=";
         public static string ApiKey = "МійКлюч";
         public static string ApiHost = "spotify23.p.rapidapi.com";
     }
